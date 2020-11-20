5e0edfbb81783258df33788dfbbf931015b4ec85
Steven Topazio <topazio.steven@gmail.com>
2020-11-20 18:47:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,9 @@ class Patient
   end
   
   def appointments 
-    Appointment.all.select {|appointment| appointment.patient == self} 
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
   
   def doctors
