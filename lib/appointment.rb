3273b8ad470258eed26c5ca8b92aff09058d9bd5
Steven Topazio <topazio.steven@gmail.com>
2020-11-20 17:56:17 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,7 +4,7 @@ class Appointment
  
  @@all = []
  
- def initialize(patient, date, doctor)
+ def initialize(patient= "", date, doctor)
    @patient = patient 
    @date = date
    @doctor = doctor 
